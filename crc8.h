5d999a3cb5b1493cbe7c2e5dcda5f6230013aab1
Gedeon Sainrival <41795342+nightfwm@users.noreply.github.com>
2025-06-15 22:20:33 -0400

    add table lookup

---
+++ crc8.h
@@ -0,0 +1,10 @@
+#ifndef CRC8_IMPL_LIBRARY_H
+#define CRC8_IMPL_LIBRARY_H
+
+typedef unsigned char uint8;
+typedef unsigned long uint32;
+
+void hello(void);
+uint8 CRC8Compute(const uint8 *data, uint32 len, uint8 initVal);
+
+#endif //CRC8_IMPL_LIBRARY_H
--- main.c
+++ main.c
@@ -1,6 +1,16 @@
-#include "library.h"
+#include <assert.h>
+#include <stdio.h>
+
+#include "crc8.h"
 
 int main(void) {
-    hello();
+    uint8 crc;
+
+    const uint8 data1[] = {0x00, 0x00, 0x00, 0x00};
+    crc = CRC8Compute(data1, 4, 0xFF);
+    crc ^= 0xFF;
+    printf("CRC = 0x%02x\n", crc);
+    assert (crc == 0x59);
+
     return 0;
 }
